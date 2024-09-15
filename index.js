489ce1baa97eec2e2afe65035583f1bf1e36cb49
KingZCoding <traenknercody@gmail.com>
2024-09-15 18:41:32 -0400

    finishing rps project

--- index.js
+++ index.js
@@ -1,76 +1,93 @@
-//creating never changing choices
 const choices = ['Rock', 'Paper', 'Scissors'];
+let playerScore = 0;
+let computerScore = 0;
+let scores = null;
+let gameOver = false;
 
-//creating a prompt for a player to enter their choices to play
-function getPlayerChoice() {
-  let playerChoice = prompt('Enter Rock, Paper, or Scissors to play!');
-  if (playerChoice) {
-    //logic to convert player input with first letter cap and rest lower and checking to make sure that playerChoice matches set choices
-    playerChoice =
-      playerChoice.charAt(0).toUpperCase() +
-      playerChoice.slice(1).toLowerCase();
-    if (choices.includes(playerChoice)) {
-      return playerChoice;
-      //if playerChoice matches we skip over else statement
-    } else {
-      //alerting player that playerChoice does not match choices and reprompts getPlayerChoice()
-      alert('You have not chosen a correct reposnse! Do it again!');
-      return getPlayerChoice();
-    }
-  }
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const btnRock = document.getElementById('rock');
+  const btnPaper = document.getElementById('paper');
+  const btnScissors = document.getElementById('scissors');
+  const btnReset = document.getElementById('reset');
+  scores = document.getElementById('score');
+  btnRock.addEventListener('click', function () {
+    playGame('Rock');
+  });
+  btnPaper.addEventListener('click', function () {
+    playGame('Paper');
+  });
+  btnScissors.addEventListener('click', function () {
+    playGame('Scissors');
+  });
+  btnReset.addEventListener('click', restartGame);
+});
 
-//creating ai choice selection
 function getComputerChoice() {
-  //creating and storing a function incase of a tie
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-//creating function to play a round of rps
 function playRound(playerChoice, computerChoice) {
-  //console logging the player choice and computer choice to start a round
   console.log(`Player Choice: ${playerChoice}`);
   console.log(`Computer Choice: ${computerChoice}`);
 
-  //checking to see if it is a tie and if so we play a new round
   if (playerChoice === computerChoice) {
-    console.log('It is a tie! Do it again!');
-    const newPlayerChoice = getPlayerChoice();
-    const newComputerChoice = getComputerChoice();
-    playRound(newPlayerChoice, newComputerChoice);
-    //round logic of game if not a tie
+    showScore('tie');
   } else if (
     (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
     (playerChoice === 'Scissors' && computerChoice === 'Paper') ||
     (playerChoice === 'Paper' && computerChoice === 'Rock')
   ) {
-    console.log('Player has won the round! Wow you beat a computer!');
-    return true;
+    playerScore++;
+    showScore('player');
   } else {
-    console.log(
-      "Computer has won the round! It's ok. It is a computer after all!"
-    );
-    return false;
+    computerScore++;
+    showScore('computer');
+  }
+  checkWinner();
+}
+
+function showScore(winner) {
+  let scoreStr = '';
+  if (winner === `tie`) {
+    scoreStr = `<p>It is a tie! Do it again! Player Score: ${playerScore} / Computer Score ${computerScore}</p>`;
+  } else if (winner === `player`) {
+    scoreStr = `<p>Player has won the round! Wow you beat a computer! Player Score: ${playerScore}/Computer Score :${computerScore}</p>`;
+  } else {
+    scoreStr = `<p>Computer has won the round! It's ok. It is a computer after all! Computer Score: ${computerScore} / Player Score: ${playerScore}</p>`;
+  }
+  scores.innerHTML += scoreStr;
+}
+
+function clearScores() {
+  playerScore = 0;
+  computerScore = 0;
+}
+
+function checkWinner() {
+  if (playerScore === 5) {
+    scores.innerHTML += `<p>Player has won with 5 points!</p>`;
+    gameOver = true;
+  } else if (computerScore === 5) {
+    scores.innerHTML += `<p>Computer has won with 5 points!</p>`;
+    gameOver = true;
   }
 }
 
-function playGame(round) {
-  //creating variables for score
-  let playerScore = 0;
-  let computerScore = 0;
+function restartGame() {
+  clearScores();
+  gameOver = false;
+  scores.innerHTML = '';
+}
 
-  //for loop to keep track of score per round
-  for (let i = 0; i < round; i++) {
-    const playerChoice = getPlayerChoice();
-    const computerChoice = getComputerChoice();
-    let result = playRound(playerChoice, computerChoice);
-
-    if (result === true) {
-      playerScore++;
-    } else {
-      computerScore++;
-    }
+function playGame(playerChoice) {
+  if (gameOver) {
+    scores.innerHTML += `<p>Game is over</p>`;
+    checkWinner();
+    return;
   }
+  const computerChoice = getComputerChoice();
+  playRound(playerChoice, computerChoice);
+
   if (playerScore > computerScore) {
     console.log(
       `Player has won! Player has ${playerScore} points and Computer has ${computerScore} points!`
@@ -82,4 +99,4 @@ function playGame(round) {
   }
 }
 
-playGame(5);
+// playGame(5);
